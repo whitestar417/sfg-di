ebc9f4862586bd479bc4df01f1765fb8ac838f9b
David Haas <Dhaas3@mail.greenriver.edu>
2021-04-23 00:09:26 -0700

    Created Default Profile

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * Created by dh on 4/22/21
  */
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService
 {
